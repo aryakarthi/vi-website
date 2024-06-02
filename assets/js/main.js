587a9ed3f20139996e9bbf85621fec4ac44c1938
aryakarthi <aryaperumal.m@gmail.com>
2024-06-03 00:51:53 +0530

    added js code for job openings accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,32 @@ const removeStyle = () => {
     });
   }
 };
+
+// accordion
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion, index) => {
+  const header = accordion.querySelector(".accordion__header");
+  const content = accordion.querySelector(".accordion__content");
+  const icon = accordion.querySelector("#accordion-icon");
+
+  header.addEventListener("click", () => {
+    const isOpen = content.style.height === `${content.scrollHeight}px`;
+
+    accordions.forEach((a, i) => {
+      const c = a.querySelector(".accordion__content");
+      const ic = a.querySelector("#accordion-icon");
+
+      if (i === index) {
+        c.style.height = isOpen ? "0px" : `${c.scrollHeight}px`;
+        ic.classList.toggle("ri-add-line", isOpen);
+        ic.classList.toggle("ri-subtract-fill", !isOpen);
+      } else {
+        c.style.height = "0px";
+        ic.classList.add("ri-add-line");
+        ic.classList.remove("ri-subtract-fill");
+      }
+    });
+  });
+});
